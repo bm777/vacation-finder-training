113666b16b2d3bf6367ab2cda9e91767cb34bb80
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-02-09 16:57:48 +0100

    Added 9 others towns

--- Fetcher_download.py
+++ Fetcher_download.py
@@ -6,14 +6,14 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 def process_download():
     d = Dataset()
-
+    list_town = ["Miami", "New York", "Las Vega", "Chicago", "Seattle", "San Francisco", "Washington","New Orleans", "Palm Springs", "San Diego", "Charleston"]
     list_dataset = ["temp", "rainfall", "snowfall", "wind", "solar"]
+    for town in list_town:
+        for dataset in list_dataset:
+            print(f"-dataset: {dataset} -town: {town}")
+            res = d.download(town, dataset)
+            d.save(res)
 
-    for dataset in list_dataset:
-        print("dataset: ", dataset)
-        res = d.download("Miami", dataset)
-        d.save(res)
-        
 
 process_download()
 
