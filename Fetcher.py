133d828fa49c0e81c00d6dee90195367fd91f7c6
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-02-19 03:43:42 +0100

    Added possibility to upload town location information from json file

--- Fetcher.py
+++ Fetcher.py
@@ -17,7 +17,7 @@ class Dataset:
         self.description = "This is a class which download dataset"
         self.town = None
         self.dataset = None
-        
+
     def download(self, town, dataset_code):
         self.town = town
         self.dataset = dataset_code
@@ -28,7 +28,7 @@ class Dataset:
         if isinstance(self.get_dataset_name(dataset_code), list):
             dataset_name0 = self.get_dataset_name(dataset_code)[0]
             dataset_name1 = self.get_dataset_name(dataset_code)[1]
-            
+
             response0 = r.get(f'https://api.dclimate.net/apiv3/grid-history/{dataset_name0}/{lat}_{lon}', headers=HEADER)
             response1 = r.get(f'https://api.dclimate.net/apiv3/grid-history/{dataset_name1}/{lat}_{lon}', headers=HEADER)
             response = self.get_norme(response0.json(), response1.json())
@@ -43,10 +43,10 @@ class Dataset:
             else:
                 response['data'] = {k:v for k,v in response['data'].items()}
             return response
-    
+
     def save(self, content):
         _path = f"dataset/{self.dataset}/{self.town}.json"
-        
+
         if not os.path.exists("dataset/"):
             os.mkdir("dataset")
             if not os.path.exists(f"dataset/{self.dataset}"):
@@ -58,18 +58,19 @@ class Dataset:
                 os.mkdir(f"dataset/{self.dataset}")
                 if os.path.exists(_path):
                     os.remove(_path)
-            
+
         if self.dataset in ["temp", "wind"]:
             daily = self.get_daily_values(content["data"], metric="avg")
         elif self.dataset in ["rainfall", "snowfall", "solar"]:
             daily = self.get_daily_values(content["data"], metric="sum")
-        
+
         print(_path)
         with open(_path, 'w') as json_file:
             json.dump(daily, json_file)
         print(f"{_path} Saved.")
-    
+
     def get_lat_lon(self, name):
+        # this json data could be replaced later by a jsonf file data
         town = {
             "Miami": [25.762329613614614, -80.19114735100034],
             "New York": [40.64136425563865, -73.78201731266307],
@@ -84,7 +85,7 @@ class Dataset:
             "Charleston": [32.7725713187095, -79.91668374248282]
         }
         return town[name] if town[name]!= None else None
-    
+
     def get_dataset_name(self, name):
         dataset = {
             "wind": ["era5_land_wind_u-hourly", "era5_land_wind_v-hourly"],
@@ -94,13 +95,13 @@ class Dataset:
             "solar": "era5_land_surface_solar_radiation_downwards-hourly"
         }
         return dataset[name] if dataset[name]!= None else None
-    
-    def get_norme(self, r0, r1): 
+
+    def get_norme(self, r0, r1):
         ret = {'data': {}}
         for k,v in r0['data'].items():
             ret['data'][k] = f'{math.sqrt(float(v.split()[0])**2 + float(r1["data"][k].split()[0])**2)} {v.split()[1]}'
         return ret
-        
+
     def get_token(self):
         """
         Get a THE TOKEN FROM local storage
@@ -110,10 +111,10 @@ class Dataset:
         with open(".token") as t:
             tok = t.readline()
         return tok.strip()
-    
+
     def get_daily_values(self, timeseries, metric="avg"):
         values = {}
-        
+
         if metric == "sum":
             for k, v in timeseries.items():
                 if v is not None:
@@ -121,8 +122,8 @@ class Dataset:
                         values[k[:10]] += [float(v.split()[0])]
                     else:
                         values[k[:10]] = [float(v.split()[0])]
-            return {"data": {k:sum(v) for k,v in values.items()} } 
-        
+            return {"data": {k:sum(v) for k,v in values.items()} }
+
         # in case, metric = avg
         if metric == "avg":
             for k, v in timeseries.items():
@@ -131,7 +132,7 @@ class Dataset:
                         values[k[:10]] += [float(v.split()[0])]
                     else:
                         values[k[:10]] = [float(v.split()[0])]
-            return {"data": {k:sum(v)/len(v) for k,v in values.items()} } 
+            return {"data": {k:sum(v)/len(v) for k,v in values.items()} }
 
         return {"data": values}
 
@@ -141,4 +142,3 @@ class Dataset:
 #dataset = Dataset()
 
 #d = dataset.download("Miami", "wind")
-
